b1ae3018e1ea8c4cf84fe90d814297605a220251
katerinamant <katerinamantaraki@gmail.com>
2025-04-08 21:08:17 +0300

    add LandingPageActivity layout

---
+++ app/src/main/java/com/example/showtime/LandingPage/LandingPageActivity.java
@@ -0,0 +1,16 @@
+package com.example.showtime.LandingPage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.showtime.R;
+
+public class LandingPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_page);
+    }
+}
