f66e29e14e66807daae6583ca68f56471312e284
katerinamant <katerinamantaraki@gmail.com>
2025-04-08 23:40:57 +0300

    add HelpPageActivity layout

---
+++ app/src/main/java/com/example/showtime/HelpPage/HelpPageActivity.java
@@ -0,0 +1,16 @@
+package com.example.showtime.HelpPage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.showtime.R;
+
+public class HelpPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_page);
+    }
+}
